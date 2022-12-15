1a15b79d2fdcf8253a9abe550203a1588480554a
TaweelapTop <117077117+TaweelapTop@users.noreply.github.com>
2022-12-15 17:45:49 +0700

    Update create.blade.php

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -3,17 +3,27 @@
 {{-- TASK: add the validation errors here - with whatever HTML structure you want --}}
 {{-- in case of title/description empty, visitor should see --}}
 {{-- "The name field is required." and "The description field is required." --}}
+@if ($error->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>
+                    {{ $error }}
+                </li>
+            @endforeach
+         </ul>
+     </div>
+ @endif
 
 <form method="POST" action="{{ route('projects.store') }}">
     @csrf
-    @foreach($projects as $project)
     Title:
     <br />
-    <input type="text" name="title" value="{{ $project->title }}" />
+    <input type="text" name="title" />
     <br /><br />
     Description:
     <br />
-    <input type="text" name="description" value="{{ $project->description }}" />
+    <input type="text" name="description" />
     <br /><br />
     <button type="submit">Save</button>
     @endforeach
