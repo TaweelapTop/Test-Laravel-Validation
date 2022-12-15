1584f2585127ebb22bc215f83ef5e3e4f7cd4779
TaweelapTop <117077117+TaweelapTop@users.noreply.github.com>
2022-12-15 15:18:43 +0700

    Update create.blade.php

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -6,13 +6,15 @@
 
 <form method="POST" action="{{ route('projects.store') }}">
     @csrf
+    @foreach($projects as $project)
     Title:
     <br />
-    <input type="text" name="title" />
+    <input type="text" name="title" value="{{ $project->title }}" />
     <br /><br />
     Description:
     <br />
-    <input type="text" name="description" />
+    <input type="text" name="description" value="{{ $project->description }}" />
     <br /><br />
     <button type="submit">Save</button>
+    @endforeach
 </form>
